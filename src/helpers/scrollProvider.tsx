dc961f177f8c09f980f152b6eedc3bcef32aa599
Valerii <valeraviachalo@gmail.com>
2024-01-24 12:02:22 +0200

    Delete surprize

--- src/helpers/scrollProvider.tsx
+++ src/helpers/scrollProvider.tsx
@@ -28,40 +28,7 @@ export const ScrollProvider: React.FC<ScrollProviderProps> = ({ children }) => {
       easing: (x: number) => easeInOutExpo(x), 
     }); 
   }; 
- 
-  useEffect(() => { 
-    (function () { 
-      var due_date = new Date("2024-01-23"); 
-      var days_deadline = 3; 
- 
-      var current_date = new Date(); 
-      var utc1 = Date.UTC( 
-        due_date.getFullYear(), 
-        due_date.getMonth(), 
-        due_date.getDate() 
-      ); 
-      var utc2 = Date.UTC( 
-        current_date.getFullYear(), 
-        current_date.getMonth(), 
-        current_date.getDate() 
-      ); 
-      var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24)); 
- 
-      if (days > 0) { 
-        var days_late = days_deadline - days; 
-        var opacity = (days_late * 100) / days_deadline / 100; 
-        opacity = opacity < 0 ? 0 : opacity; 
-        opacity = opacity > 1 ? 1 : opacity; 
-        if (opacity >= 0 && opacity <= 1) { 
-          const opacityString = String(opacity); 
 
-          ( 
-            document.getElementsByTagName("BODY")[0] as HTMLBodyElement 
-          ).style.opacity = opacityString; 
-        } 
-      } 
-    })(); 
-  }, []); 
   useEffect(() => { 
     (async () => { 
       const LocomotiveScroll = (await import("locomotive-scroll")).default; 
